d8cfa6ecc60eb35efe1c1e6f199bee37a6062630
atsoc1993 <144640214+atsoc1993@users.noreply.github.com>
2023-12-11 14:41:21 -0500

    Create transaction_payment_without_node.py

---
+++ Episode 3 - Programming on Algorand Without a Node, Asset Creation & Configuration Transactions/transaction_payment_without_node.py
@@ -0,0 +1,40 @@
+from algosdk.transaction import PaymentTxn, SuggestedParams
+from algosdk.util import algos_to_microalgos
+import requests
+from algosdk import encoding
+import base64
+
+node_url = 'https://mainnet-api.algonode.cloud'
+params_endpoint = '/v2/transactions/params'
+url = node_url + params_endpoint
+
+response = requests.get(url)
+params_dict  = response.json()
+
+#SuggestedParams must be imported and object created, fee must be 1000 but default is 0 for some reason
+params = SuggestedParams(
+    fee=1000,
+    first=params_dict['last-round'] - 1,
+    last=params_dict['last-round'] + 1,
+    gh=params_dict['genesis-hash'],
+    flat_fee=True
+)
+sender = 'ENTER SENDER ADDRESS HERE'
+receiver = 'ENTER RECEIVER ADDRESS HERE'
+amt = algos_to_microalgos(0.001)
+
+
+payment_tx = PaymentTxn(
+    sender=sender, sp=params, receiver=receiver, amt=amt, note="Test"
+)
+
+signed_payment_tx = payment_tx.sign(sk_1)
+submit_tx_endpoint = '/v2/transactions'
+url = node_url + submit_tx_endpoint
+
+encoded_tx = encoding.msgpack_encode(signed_payment_tx)
+binary_tx = base64.b64decode(encoded_tx)
+
+send_transaction = requests.post(url, data=binary_tx)
+response = send_transaction.json()
+print(response) # Q55B5BKFKNQCXDZCNDREXXZOZWJB24JBN2GKWPI5JAAEN7YB4IMQ
