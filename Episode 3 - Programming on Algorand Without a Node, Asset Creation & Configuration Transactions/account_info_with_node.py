2f4cd9522ce1b140c993e1fb0395fd0e714d1310
atsoc1993 <144640214+atsoc1993@users.noreply.github.com>
2023-12-11 14:32:18 -0500

    Update account_info_with_node.py

--- Episode 3 - Programming on Algorand Without a Node, Asset Creation & Configuration Transactions/account_info_with_node.py
+++ Episode 3 - Programming on Algorand Without a Node, Asset Creation & Configuration Transactions/account_info_with_node.py
@@ -1,1 +1,11 @@
+from algosdk.v2client import *
 
+algod_token = '9225f0a6a3bb74686ed65bc16e06705df9f39a704b77f3c5c6950ca3051431eb'
+algod_port = 'http://127.0.0.1:8080'
+
+algod_client = algod.AlgodClient(algod_token, algod_port)
+
+address = 'CQLCW2F3ZTDYQUWPJ2YEZ4HHFU6CLBYZLKV42JK6L6OBXGEBAGEAULQA6E'
+account_info_for_address = algod_client.account_info(address)
+
+print(account_info_for_address)
