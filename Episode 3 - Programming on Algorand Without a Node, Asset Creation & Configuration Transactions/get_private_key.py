c9673c9f7fde534ea6806509bb86d320ce681b86
atsoc1993 <144640214+atsoc1993@users.noreply.github.com>
2023-12-12 20:27:23 -0500

    Update get_private_key.py

--- Episode 3 - Programming on Algorand Without a Node, Asset Creation & Configuration Transactions/get_private_key.py
+++ Episode 3 - Programming on Algorand Without a Node, Asset Creation & Configuration Transactions/get_private_key.py
@@ -6,6 +6,12 @@ algorand_private_key, algorand_address = account.generate_account()
 #Convert private key to mnemonic phrase to enter into wallet applications like Pera/Defly
 mnemonic_phrase = mnemonic.from_private_key(algorand_private_key)
 
+#Convert mnemonic phrase to private key
+#A mnemonic phrase is 25 words long
+mnemonic_phrase_example = "one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen twenty twentyone twentytwo twentythree twentyfour twentyfive"
+private_key_from_mnemonic = mnemonic.to_private_key(mnemonic_phrase_example)
+
+print(private_key_from_mnemonic) # Prints private key obtained from mnemonic phrase
 print(algorand_address) # Prints address
 print(algorand_private_key) # Prints private key
 print(mnemonic_phrase) # Prints mnemonic phrase
